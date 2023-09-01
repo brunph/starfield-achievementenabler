993a3e8d1a8e16d6aee2d846069cc0a2fe30f28e
brunph <48178956+brunph@users.noreply.github.com>
2023-09-01 16:26:50 +0200

    First Commit

---
+++ src/Main.cpp
@@ -0,0 +1,36 @@
+#include "CommonIncludes.hpp"
+
+mem::MemoryPatch saveModdedPatch;
+void entryPoint() {
+    std::this_thread::sleep_for(10s); // ensure everything has loaded
+
+    static auto isSaveModded = mem::scanForPtr("40 53 48 83 EC ? C6 44 24 ? ? 48 8B D9 84 D2");
+
+    saveModdedPatch.init(isSaveModded, BYTE_ARRAY("B0 00 C3")).applyPatch();
+}
+
+BOOL APIENTRY DllMain(const HMODULE hModule, const DWORD reasonForCall, [[maybe_unused]] LPCVOID lpReserved) {
+    if (reasonForCall == DLL_PROCESS_ATTACH) {
+        DisableThreadLibraryCalls(hModule);
+        // MessageBoxA(nullptr, "test", "test", MB_OK);
+
+        // LoadLibraryA("iphlpapi.dll");
+        if (const auto handle = CreateThread(
+                nullptr, 0,
+                [](LPVOID mod) -> DWORD {
+                    try {
+                        entryPoint();
+                    } catch (...) {
+                        MessageBoxA(nullptr, "An exception occured while enabling achievements", "starfield-achievementsenabler", MB_OK);
+                    }
+
+                    return 1;
+                },
+                hModule, 0, nullptr);
+            handle != nullptr) {
+            CloseHandle(handle);
+        }
+    }
+
+    return TRUE;
+}
---
+++ src/memory/Include.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "ByteArray.hpp"
+#include "Memory.hpp"
+#include "MemoryPatch.hpp"
