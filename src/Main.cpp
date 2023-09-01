6a8b778bbba9770eb9a7919a71758847af3cf3dc
brunph <48178956+brunph@users.noreply.github.com>
2023-09-01 16:27:40 +0200

    Removed debug stuff

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,7 +2,7 @@
 
 mem::MemoryPatch saveModdedPatch;
 void entryPoint() {
-    std::this_thread::sleep_for(10s); // ensure everything has loaded
+    std::this_thread::sleep_for(8s); // ensure everything has loaded
 
     static auto isSaveModded = mem::scanForPtr("40 53 48 83 EC ? C6 44 24 ? ? 48 8B D9 84 D2");
 
@@ -12,9 +12,7 @@ void entryPoint() {
 BOOL APIENTRY DllMain(const HMODULE hModule, const DWORD reasonForCall, [[maybe_unused]] LPCVOID lpReserved) {
     if (reasonForCall == DLL_PROCESS_ATTACH) {
         DisableThreadLibraryCalls(hModule);
-        // MessageBoxA(nullptr, "test", "test", MB_OK);
 
-        // LoadLibraryA("iphlpapi.dll");
         if (const auto handle = CreateThread(
                 nullptr, 0,
                 [](LPVOID mod) -> DWORD {
